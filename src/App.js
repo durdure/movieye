aaaf4c0b46c6b1e588b2973f240ca6d261afdb3d
Duresa <duresaesheu2001@gmail.com>
2024-04-28 12:19:57 +0300

    movieye

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 import Header from "./components/header/Header"
 
 function App() {
   return (
-    <>
+   <Router>
       <Header/>
-    </>
+   </Router>
   )
+  
 }
 
 export default App
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,49 @@
-import React from "react"; 
+import React, { useState } from "react"; 
 import './header.css'
-
+import { Link } from "react-router-dom";
+import logo from "./logo512.png"
 
 const Header =() => {
+    const [Mobile, setMobile] = useState(false)
     return(
         <>
         <header>
-            <div>Duresa</div>
+            <div className="container flexSB">
+                <nav className="flexSB">
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                    </div>
+                    <ul className={Mobile ? "navMenu-list" : 'flexSB'} onclick={()=> setMobile(false)}>
+                        <li><Link to="/">Home</Link></li>
+                        <li>
+                        <Link to="/series">Series</Link>
+                        </li>
+                        <li>
+                        <Link to="/movies">Movies</Link>
+                        </li>
+                        <li>
+                        <Link to="/pages">Pages</Link>
+                        </li>
+                        <li>
+                        <Link to="/pricing">Pricing</Link>
+                        </li>
+                        <li>
+                        <Link to="/contact">Contact</Link>
+                        </li>                       
+                    </ul>
+                    <button className="toggle" onclick={()=>setMobile(!Mobile)}>
+                        {Mobile ? <i className="fa fa-times"></i>: 
+                        <i className="fa fa-bars"></i>}
+                    </button>
+                </nav>
+                <div className="account flexSB">
+                    <i className="fa fa-search"></i>
+                    <i className="fa fa-bell"></i>
+                    <i className="fa fa-user"></i>
+                    <button>Subscribe Now</button>
+                </div>
+               
+           </div>
         </header>
         </>
     )
