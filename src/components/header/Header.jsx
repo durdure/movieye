19e0f96c6c927ea5ef3bfc45e3b6b1b13560e188
Duresa <duresaesheu2001@gmail.com>
2024-04-28 09:48:41 +0300

    movieye

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"; 
+import './header.css'
+
+
+const Header =() => {
+    return(
+        <>
+        <header>
+            <div>Duresa</div>
+        </header>
+        </>
+    )
+}
+
+export default Header
