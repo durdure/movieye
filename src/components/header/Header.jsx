8b7c638d5626df67ea0f8d5ae88309e6f6a6c122
durdure <duresaeshetu2001@gmail.com>
2024-05-12 18:29:23 +0300

    cool

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import "./header.css"
-import logo from '../../images/Alexander-removebg.png'
+import logo from '../../images/logomain.png'
 
 const Header = () => {
   const [Mobile, setMobile] = useState(false)
@@ -8,40 +8,35 @@ const Header = () => {
     <>
       <header>
         <div className='container flexSB'>
+
         <div className='logo'>
               <img src= {logo} alt='' />
+              <h1>Movieye</h1>
          </div>
-          <nav className='flexSB'>
+
+          <nav className='nav'>
             
             <ul className={Mobile ? "navMenu-list" : "flexSB"} onClick={() => setMobile(false)}>
-              <li>
                 <a href='/home'>Home</a>
-              </li>
-              <li>
                 <a href='/series'>Series</a>
-              </li>
-              <li>
                 <a href='/movies'>Movies</a>
-              </li>
-              <li>
                 <a href='/pages'>Pages</a>
-              </li>
-              <li>
                 <a href='/pricing'>Pricing</a>
-              </li>
-              <li>
                 <a href='/contact'>Contact</a>
-              </li>
+              <span></span>
             </ul>
             <button className='toggle' onClick={() => setMobile(!Mobile)}>
               {Mobile ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}
             </button>
           </nav>
+
+
+
           <div className='account flexSB'>
             <i className='fa fa-search'></i>
             <i class='fas fa-bell'></i>
             <i className='fas fa-user'></i>
-            <button>Subscribe Now</button>
+            <button>Log in</button>
           </div>
         </div>
       </header>
