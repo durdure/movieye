f518beadb2740142ffe86c61470a10e23b02b60f
durdure <duresaeshetu2001@gmail.com>
2024-05-29 16:17:49 +0300

    cool

--- src/components/upComing/uCard.jsx
+++ src/components/upComing/uCard.jsx
@@ -5,19 +5,19 @@ import './UpComming.css'
 const UCard = ({item: {id, cover, name, time }}) => {
     return (
         <div className="card-group"> 
-        <div className="card">
-            <img src={cover} alt="" />
+            <div className="card">
+                <img src={cover} alt="" />
 
-            <div className="layer"></div>
-            <div className="info">
-                <h3>{name}</h3>
-                <span> {time} </span>
-                <br />
-                <button className="primary-btn">
-                    <i className="fa fa-play"></i></button>
+                <div className="layer"></div>
+                <div className="info">
+                    <h3>{name}</h3>
+                    <span> {time} </span>
+                    <br />
+                    <button className="primary-btn">
+                        <i className="fa fa-play"></i></button>
+                </div>
             </div>
-        </div>
-        </div>
+       </div>
     )
 }
 
